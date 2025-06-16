d4b4641f11ad0020d1386772b50cfecec2b1fcea
tsuchihashi <tsuchihashi@github.com>
2025-06-16 10:38:41 +0900

    revertによる修正の体験

--- .log/.history/semonster/Main_20250616103728.java
+++ .log/.history/semonster/Main_20250616103728.java
@@ -2,7 +2,10 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello SEMonster");
 
+
     Monster monster = new Monster();
     System.out.println(monster);
+=======
+>>>>>>> parent of 1b7cdef (summon monster処理を実装)
   }
 }
