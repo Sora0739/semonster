76d823c9fd40eed77be39bd7ee907938d6fee72a
tsuchihashi <tsuchihashi@github.com>
2025-06-16 10:31:09 +0900

    フルフルフルフル

--- .log/.history/semonster/Main_20250616101112.java
+++ .log/.history/semonster/Main_20250616101112.java
@@ -2,8 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello SEMonster");
 
-    System.out.println("furufurufurufurufurufurufurufuru");
-
     Monster monster = new Monster();
     System.out.println(monster);
   }
